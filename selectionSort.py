44cdf15a614cae760bbebbcd11d675190a7cc233
gatewj <gatewj@gmail.com>
2022-04-26 20:52:14 -0400

    Add files via upload

---
+++ selectionSort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 20:32:49 2022
+
+@author: Jambo
+"""
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([5,3,6,2,10]))
