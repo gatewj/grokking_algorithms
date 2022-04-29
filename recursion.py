a086ae022f4d1dcadbb010be8b5b9d24c7429422
gatewj <gatewj@gmail.com>
2022-04-28 21:29:06 -0400

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 07:24:04 2022
+
+@author: Jambo
+"""
+def countdown(i):
+    print(i)
+    if i <= 1:
+        return
+    else:
+        countdown(i-1)
+        
+        
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
